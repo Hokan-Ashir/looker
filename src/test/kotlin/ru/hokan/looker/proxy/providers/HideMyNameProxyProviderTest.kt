c87a1ded96a62306d59ae4f30eb63267aedf5aee
aachkasov <a.achkasov@cardsmobile.ru>
2019-12-18 03:28:10 +0300

    switching to kotlinTest with JUnit 5, not working for Gradle (known issue for last IDEA's version): wip

--- src/test/kotlin/ru/hokan/looker/proxy/providers/HideMyNameProxyProviderTest.kt
+++ src/test/kotlin/ru/hokan/looker/proxy/providers/HideMyNameProxyProviderTest.kt
@@ -1,17 +1,16 @@
 package ru.hokan.looker.proxy.providers
 
+import io.kotlintest.extensions.TestListener
 import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
+import io.kotlintest.spring.SpringListener
 import org.junit.Before
-import org.junit.Test
-import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
 import ru.hokan.looker.LookerApplication
 
-@RunWith(SpringRunner::class)
 @SpringBootTest(classes = [LookerApplication::class])
-class HideMyNameProxyProviderTest {
+class HideMyNameProxyProviderTest : StringSpec() {
 
     @Autowired
     lateinit var proxyProvider: HideMyNameFileProxyProvider
@@ -21,11 +20,17 @@ class HideMyNameProxyProviderTest {
         proxyProvider.register()
     }
 
-    @Test
-    fun `should extract proxies from file-like proxy provider`() {
-        val proxyList = proxyProvider.getProxyList()
-        val expectedProxiesList = listOf("34.34.11.23", "45.66.77.123", "123.142.181.11")
-        val proxyIPList = proxyList.map { it.IP }
-        proxyIPList shouldBe expectedProxiesList
+    override fun listeners(): List<TestListener> {
+        return listOf(SpringListener)
+    }
+
+    init {
+
+        "should extract proxies from file-like proxy provider" {
+            val proxyList = proxyProvider.getProxyList()
+            val expectedProxiesList = listOf("34.34.11.23", "45.66.77.123", "123.142.181.11")
+            val proxyIPList = proxyList.map { it.IP }
+            proxyIPList shouldBe expectedProxiesList
+        }
     }
 }
